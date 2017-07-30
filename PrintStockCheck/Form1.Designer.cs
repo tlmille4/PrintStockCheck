a7511be80de070b144c3cede1499ac6044e0b62a
Tyler Miller <tlmille4@svsu.edu>
2017-07-29 22:22:43 -0400

    Add project files.

---
+++ PrintStockCheck/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PrintStockCheck
+{
+    partial class stockCheckMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInFile
+            // 
+            this.btnInFile.Location = new System.Drawing.Point(22, 51);
+            this.btnInFile.Name = "btnInFile";
+            this.btnInFile.Size = new System.Drawing.Size(103, 23);
+            this.btnInFile.TabIndex = 0;
+            this.btnInFile.Text = "Select Input File";
+            this.btnInFile.UseVisualStyleBackColor = true;
+            this.btnInFile.Click += new System.EventHandler(this.btnInFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(131, 53);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(317, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // stockCheckMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 294);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnInFile);
+            this.Name = "stockCheckMain";
+            this.Text = "Print Stock Checker";
+            this.Load += new System.EventHandler(this.stockCheckMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
+
---
+++ PrintStockCheck/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintStockCheck
+{
+    public partial class stockCheckMain : Form
+    {
+        String fileLocation = "";
+
+        public stockCheckMain()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            String filepath;
+            filepath = openFileDialog.FileName;
+
+        }
+
+        private void btnInFile_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+                fileLocation = openFileDialog.FileName;
+            }
+        }
+
+        private void stockCheckMain_Load(object sender, EventArgs e)
+        {
+
+            String currSku = "5a8utyre";
+            String[] images = Directory.GetFiles("C:\\Users\\Tyler PC\\Downloads", "*.jpg");
+            
+
+            if (images.Length > 0)
+            {
+                String currDirFile = Path.GetFileNameWithoutExtension(images[0]);
+                Console.WriteLine(currDirFile);
+
+
+                if (currSku == currDirFile)
+                {
+                    //TO DO LOGIC IN EXCEL
+                    Console.WriteLine("IT WAS A MATCH");
+                }
+                else
+                {
+                    //TO DO LOGIC EXTRAPOLATE FROM SKU AND CYCLE THROUGH ALL FILES
+                    Console.WriteLine("NOT A MATCH");
+                }
+                
+
+            }
+            
+        }
+    }
+}
