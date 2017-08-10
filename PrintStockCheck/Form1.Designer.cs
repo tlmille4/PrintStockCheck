b1745b789f61dddac71a317a6514735fa59a9c82
Tyler Miller <tlmille4@svsu.edu>
2017-08-09 22:18:53 -0400

    tlmille4: Updated and included more functionality and error checking. V1 working

--- PrintStockCheck/Form1.Designer.cs
+++ PrintStockCheck/Form1.Designer.cs
@@ -31,6 +31,13 @@
             this.btnInFile = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.grpStatus = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblPercentComplete = new System.Windows.Forms.Label();
+            this.grpStatus.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnInFile
@@ -55,16 +62,77 @@
             this.txtFilePath.Size = new System.Drawing.Size(317, 20);
             this.txtFilePath.TabIndex = 1;
             // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(199, 109);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search File";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(62, 41);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 29);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // grpStatus
+            // 
+            this.grpStatus.Controls.Add(this.lblStatus);
+            this.grpStatus.Controls.Add(this.lblPercentComplete);
+            this.grpStatus.Controls.Add(this.progressBar1);
+            this.grpStatus.Location = new System.Drawing.Point(22, 138);
+            this.grpStatus.Name = "grpStatus";
+            this.grpStatus.Size = new System.Drawing.Size(426, 126);
+            this.grpStatus.TabIndex = 4;
+            this.grpStatus.TabStop = false;
+            this.grpStatus.Text = "Scan Status";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(0, 83);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(420, 29);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Scan Not Yet Started";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPercentComplete
+            // 
+            this.lblPercentComplete.AutoSize = true;
+            this.lblPercentComplete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercentComplete.Location = new System.Drawing.Point(357, 46);
+            this.lblPercentComplete.Name = "lblPercentComplete";
+            this.lblPercentComplete.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPercentComplete.Size = new System.Drawing.Size(29, 18);
+            this.lblPercentComplete.TabIndex = 4;
+            this.lblPercentComplete.Text = "0%";
+            // 
             // stockCheckMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 294);
+            this.Controls.Add(this.grpStatus);
+            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtFilePath);
             this.Controls.Add(this.btnInFile);
             this.Name = "stockCheckMain";
             this.Text = "Print Stock Checker";
             this.Load += new System.EventHandler(this.stockCheckMain_Load);
+            this.grpStatus.ResumeLayout(false);
+            this.grpStatus.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +143,12 @@
         private System.Windows.Forms.Button btnInFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnSearch;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox grpStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblPercentComplete;
     }
 }
 
--- PrintStockCheck/Form1.cs
+++ PrintStockCheck/Form1.cs
@@ -8,24 +8,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
 
 namespace PrintStockCheck
 {
     public partial class stockCheckMain : Form
     {
-        static string imgDir = "C:\\Test";
+        static string imgDir = "C:\\Users\\Tyler PC\\Dropbox\\Camera Uploads";
         string fileLocation = "";
         int currImg = 0;
-        string currSku = "5a8utyre";
-        string[] images = Directory.GetFiles(imgDir, "*.jpg");
+        string currSku = "Pareto Chart Information";
+        
+        
+        //string[] images = Directory.GetFiles(imgDir, "*.jpg");
+
+        
+     
+
+        //var files = Directory.GetFiles("C:\\path", "*.*", SearchOption.AllDirectories)
+        //            .Where(s => s.EndsWith(".mp3") || s.EndsWith(".jpg"));
 
         public stockCheckMain()
         {
             InitializeComponent();
+            openFileDialog.Filter = "CSV Files (*.csv)| *.csv";
+            openFileDialog.Filter = "Excel Files (*.xls)| *.xls";
+            openFileDialog.Filter = "Excel Files (*.xlsx)| *.xlsx";
+
         }
 
         private void openFileDialog_FileOk(object sender, CancelEventArgs e)
         {
+
             String filepath;
             filepath = openFileDialog.FileName;
 
@@ -42,43 +57,227 @@ namespace PrintStockCheck
 
         private void stockCheckMain_Load(object sender, EventArgs e)
         {
-            checkSkuMatch();
+            
  
         }
 
         private void checkSkuMatch()
         {
-            if (images.Length > 0 && currImg < images.Length)
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            xlApp.Visible = true;
+
+
+            try
             {
+                Workbook wb = xlApp.Workbooks.Open(fileLocation);
+                // Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+                Worksheet ws = (Worksheet)wb.Worksheets[1];
+                Range usedRange = ws.UsedRange;
+                int totalRows = ws.UsedRange.Rows.Count;
+                Console.WriteLine("Total Rows ----- " + totalRows);
 
-                String currDirFile = Path.GetFileNameWithoutExtension(images[currImg]);
-                Console.WriteLine(currDirFile);
+                int currRow = 1;
+                double percentComplete;
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = totalRows;
+
+                lblStatus.Text = "Scanning Excel Worksheet .. Please Wait .. ";
+                foreach (Range row in usedRange.Rows)
+                {
 
 
-                if (currSku == currDirFile)
+                    string skuData = row.Cells[1, 1].Value.ToString();
+
+
+                    //EXTRAPOLATE SKU
+                    string[] tempSku = skuData.Split('-');
+
+                    if (tempSku.Length >= 2)
+                    {
+                        string searchSku = tempSku[1];
+                        Console.WriteLine(searchSku);
+                        string[] images = Directory.GetFiles(imgDir, "*" + searchSku + "*.*");
+                        Console.WriteLine("Img Length: " + images.Length);
+                        if (images.Length > 0)
+                            checkDirectory(currRow, ws, row, images, searchSku);
+                        else
+                            Console.WriteLine("NOT IN FILE SYSTEM");
+
+                    }
+                    this.progressBar1.Value = currRow;
+                    this.progressBar1.Update();
+
+
+
+
+                    percentComplete = (double)currRow / totalRows * 100;
+
+                    lblPercentComplete.Text = Math.Round(percentComplete, 1).ToString() + "%";
+                    this.Refresh();
+                    currRow++;
+
+                }
+
+                lblStatus.ForeColor = Color.Green;
+                lblStatus.Text = "Scan Complete";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Whoops! The file you're trying to use is either non-existant or invalid. Please try another file. \n Error: " + ex);
+
+            }
+
+
+
+        }
+
+        public void checkDirectory(int currRow, Worksheet ws, Range row, string[] images, string skuData)
+        {
+            string currDirFile;
+           // string indata;
+
+            for (int i = 0; i < images.Length; i++)
+            {
+                currDirFile = Path.GetFileNameWithoutExtension(images[i]);
+                //Console.WriteLine(currDirFile);
+                
+                //Console.WriteLine(indata);
+                if (skuData == currDirFile)
                 {
                     //TO DO LOGIC IN EXCEL
-                    Console.WriteLine("IT WAS A MATCH");
+                    Console.WriteLine("EXACT MATCH");
+                    ws.Cells[currRow, 6] = "Scanned";
+                   // Console.WriteLine(indata);
                 }
                 else
                 {
                     //TO DO LOGIC EXTRAPOLATE FROM SKU AND CYCLE THROUGH ALL FILES
-                    Console.WriteLine("NOT EXACT MATCH");
+
+                    //Checking that current Excel sku contains the current directory file and that the directory file isn't a small thumbnail image
+                    if (currDirFile.Contains(skuData) && !currDirFile.Contains("jpg"))
+                    {
+                        //THIS MEANS ITS A VALID MATCH, SEND TO FUNCTION TO PROCESS EXCEL
+                        Console.WriteLine("match");
+                        ws.Cells[currRow, 6] = "Possible Match";
+
+
+
+                        // Console.WriteLine(indata);
+                    }
+                    else
+                    {
+                        Console.WriteLine("NOT  MATCH");
+
+                    }
+                }
+            }
+
+            /*
+            if (images.Length > 0 && currImg < images.Length)
+            {
+
+                
+                String currDirFile = Path.GetFileNameWithoutExtension(images[currImg]);
+                Console.WriteLine(currDirFile);
+                string indata = row.Cells[1, 1].Value.ToString();
+                Console.WriteLine(indata);
+                if (indata == currDirFile)
+                {
+                    //TO DO LOGIC IN EXCEL
+                    Console.WriteLine("IT WAS A MATCH");
+                    Console.WriteLine(indata);
+                }
+                else
+                {
+                    //TO DO LOGIC EXTRAPOLATE FROM SKU AND CYCLE THROUGH ALL FILES
+
                     //Checking that current Excel sku contains the current directory file and that the directory file isn't a small thumbnail image
                     if (currSku.Contains(currDirFile) && !currDirFile.Contains("jpg"))
                     {
                         //THIS MEANS ITS A VALID MATCH, SEND TO FUNCTION TO PROCESS EXCEL
-                        Console.WriteLine("BITCH BETTA HAVE MY HONEY");
+                        Console.WriteLine("match");
+
+                        Console.WriteLine(indata);
                     }
                     else
                     {
+                        Console.WriteLine("NOT  MATCH");
                         currImg++;
-                        checkSkuMatch();
+                        checkDirectory(row);
                     }
                 }
             }
             else
-                Console.WriteLine("No .JPG files in directory: " + imgDir);
+                Console.WriteLine("End of directory search");
+                */
         }
+
+        public void MarkExcelFile()
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlApp == null)
+            {
+                Console.WriteLine("EXCEL could not be started. Check that your office installation and project references are correct.");
+                return;
+            }
+            xlApp.Visible = true;
+
+            Workbook wb = xlApp.Workbooks.Open(fileLocation);
+           // Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            Worksheet ws = (Worksheet)wb.Worksheets[1];
+            Range usedRange = ws.UsedRange;
+
+            foreach (Range row in usedRange.Rows)
+            {
+                    string indata = row.Cells[1,1].Value.ToString();
+                   // Console.WriteLine(indata);
+            }
+
+
+
+            if (ws == null)
+            {
+                Console.WriteLine("Worksheet could not be created. Check that your office installation and project references are correct.");
+            }
+
+            // Select the Excel cells, in the range c1 to c7 in the worksheet.
+            Range aRange = ws.get_Range("C1", "C7");
+
+            if (aRange == null)
+            {
+                Console.WriteLine("Could not get a range. Check to be sure you have the correct versions of the office DLLs.");
+            }
+
+            // Fill the cells in the C1 to C7 range of the worksheet with the number 6.
+            Object[] args = new Object[1];
+            args[0] = 6;
+            aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+
+            // Change the cells in the C1 to C7 range of the worksheet to the number 8.
+            aRange.Value2 = 8;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                checkSkuMatch();
+            }
+            catch(FileNotFoundException ex)
+            {
+                MessageBox.Show("Whoops! The file you're trying to use is either non-existant or invalid. Please try another file. \n Error: " + ex);
+
+            }
+            
+           
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
     }
-}
+  }
